8ed25bb5814813b46dca64db7e80ed3214432262
stilefano <stilefano@gmail.com>
2012-11-22 13:08:54 +0100

    full screen colored grid

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ $(function() {
 	//size of canvas
 	// var cw = bw + (p*2) + 1;
 	// var ch = bh + (p*2) + 1;
-	var bs = 100;
+	var bs = 5;
 	var scrollSize = 10;
 
 	var canvas = $('<canvas/>').attr({
@@ -24,54 +24,71 @@ $(function() {
 			height : bh
 		})
 
-		//context.fillRect(100, 100, 100, 100);
-		//context.clearRect(45,45,60,60);
-		//context.fill();
-
-		var colorArray = ["00", "08", "10", "18", "20", "28", "30", "38", "40", "48", "50", "58", "60", "68", "70", "78", "80", "88", "90", "98", "A0", "A8", "B0", "B8", "C0", "C8", "D0", "D8", "E0", "E8", "F0", "F8", "FF"]
-
-		/*for ( i = 0; i < 20; i++) {
-		 for ( j = 0; j < 20; j++) {
-		 //context.fillStyle = 'rgb(' + Math.floor(255 - 10 * i) + ',' + Math.floor(255 - 10 * j) + ',0)';
-
-		 //context.fillRect(j * 100, i * 100, 100, 100);
-		 console.log("i=", i, " ***", "j=", j)
-		 }
-		 }*/
+		var colorArrayR = ["00", "08", "10", "18", "20", "28", "30", "38", "40", "48", "50", "58", "60", "68", "70", "78", "80", "88", "90", "98", "A0", "A8", "B0", "B8", "C0", "C8", "D0", "D8", "E0", "E8", "F0", "F8", "FF"]
+		var colorArrayG = colorArrayR.slice(0);
+		var colorArrayB = colorArrayR.slice(0);
+		
+		l=0;
 		i=-1;
-		var mainColor=2;
+		setReached=0;
+		var mainColor=colorArrayR[15];
 		for (var x=0;x<=bw;x+=bs){
 			i++;
+			
+			if(colorArrayB[i]==undefined){
+				colorArrayB.reverse();
+				i=1;
+			}
 			k=0;
+			if(l%2==0)colorArrayG.reverse();
+			l=1;
+				
 			for (var y = 0; y <= bh; y += bs) {
 					k++;
-					var color = colorArray[mainColor] + colorArray[k] + colorArray[i];
+						
+					if(colorArrayG[k]==undefined){
+						colorArrayG.reverse();
+						k=1;	
+						l++;		
+					}
+
+					var color = mainColor + colorArrayG[k] + colorArrayB[i];
 					context.fillStyle = "#" + color
-					context.fillRect(x, y, 100, 100);
-					console.log("i=",i,"****","k=",k,"****","x=",x,"****","y=",y,"****","color=",color);
+					context.fillRect(x, y, bs, bs);
+			}
+		}
+		
+		
+		/**********
+		function colorObject (colorR,colorG,colorB){			
+			this.r = colorR;
+			this.g = colorG;
+			this.b = colorB;
 			}
 		
+		for (var key in colorArray){
+			var color = new colorObject(colorArray[key],colorArray[key],colorArray[key])
+			colorBundle = [];
+			colorBundle.push("#"+color.r+color.g+color.b);
+			console.log(colorBundle,colorBundle[0]);
+		}	
+		
+		
+		console.log(color)
+		
+		for (var x=0;x<=bw;x+=bs){
+			for (var y = 0; y <= bh; y += bs) {
+				i=0;
+				i++;
+				console.log(colorBundle)
+				context.fillStyle = colorBundle
+				//console.log(context.fillStyle,"#"+color.r)
+				context.fillRect(x, y, bs, bs);
+			}
 		}
-
-
-		/*for (var x = 0; x <= bw; x += bs) {
-		 context.moveTo(0.5 + x + p, p);
-		 context.lineTo(0.5 + x + p, bh + p);
-		 }
-
-		 for (var x = 0; x <= bh; x += bs) {
-		 context.moveTo(p, 0.5 + x + p);
-		 context.lineTo(bw + p, 0.5 + x + p);
-		 }
-
-		 context.strokeStyle = "black";
-		 context.stroke();*/
+		*******/
 
 	}
 
-	drawBoard();
-
-	// $(window).resize(function() {
-		// drawBoard();
-	// })
+	drawBoard();
 })
